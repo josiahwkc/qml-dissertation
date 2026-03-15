248cff25020040cf62018518f586a2111b6123d0
Josiah <josiahchan04@gmail.com>
2026-03-15 21:23:19 +0000

    Update configurations

--- src/main.py
+++ src/main.py
@@ -8,27 +8,29 @@ Description:
   Adjust the configuration variables below to run different tests.
 """
 
+# %%
+# EXECUTE
 from experiment import ExperimentRunner
 from data_manager import CSVDataManager, AdhocDataManager, SyntheticDataManager
 
 
-# GLOBAL CONFIGURATION
-NUM_DIMS = 4          # Number of PCA dimensions (and Qubits)
-NUM_TRIALS = 5        # Number of random seeds to average over (min 5 for stats)
+# Configs
+NUM_DIMS = 5          # Number of PCA dimensions (and Qubits)
+NUM_TRIALS = 10 #30        # Number of random seeds to average over (min 5 for stats)
 N_CLASS = 2           # Binary classification
-FIXED_SIZE = 100       # Default training size for non-'size' experiments
+FIXED_SIZE = 100      # Default training size for non-'size' experiments
 
 # Choose experiment mode: 
 # 'size', 'imbalance', 'feature_complexity', 'margin', 'clusters', or 'noise'
-EXPERIMENT_MODE = 'feature_complexity'
+EXPERIMENT_MODE = 'clusters'
 
 
 def main():
-    print(f"Initializing Experiment Runner in '{EXPERIMENT_MODE}' mode...")
+    print(f"Initialising Experiment Runner in '{EXPERIMENT_MODE}' mode...")
     runner = ExperimentRunner()
 
     if EXPERIMENT_MODE == 'size':
-        csv = CSVDataManager(filename="breast-cancer.csv", target_col="diagnosis", num_dims=NUM_DIMS, n_class=N_CLASS)
+        csv = CSVDataManager(filename="fashion-mnist_train.csv", target_col="label", num_dims=NUM_DIMS, n_class=N_CLASS)
         runner.run_experiment(
             mode='size', data_manager=csv, num_dims=NUM_DIMS, num_trials=NUM_TRIALS, 
             experiment_values=[20, 40, 60, 80, 100]
@@ -44,7 +46,7 @@ def main():
     elif EXPERIMENT_MODE == 'feature_complexity':
         runner.run_experiment(
             mode='feature_complexity', data_manager=None, num_dims=NUM_DIMS, num_trials=NUM_TRIALS, 
-            experiment_values=[2, 6, 10, 14, 18], fixed_size=FIXED_SIZE
+            experiment_values=[1, 2, 3, 4, 5], fixed_size=FIXED_SIZE
         )
         
     elif EXPERIMENT_MODE == 'margin':
@@ -64,6 +66,7 @@ def main():
             mode='noise', data_manager=None, num_dims=NUM_DIMS, num_trials=NUM_TRIALS, 
             experiment_values=[0.0, 0.05, 0.10, 0.15, 0.20], fixed_size=FIXED_SIZE
         )
-
+     
 if __name__ == "__main__":
     main()
+# %%
