d789befc3a75165bbd1e6032c60bad90b3fa4142
Josiah <josiahchan04@gmail.com>
2026-04-04 16:02:31 +0100

    refactor: initialise datasets creation/loading in main.py, decoupled from run_experiment

--- src/main.py
+++ src/main.py
@@ -11,7 +11,6 @@ Description:
 # %%
 # EXECUTE
 from experiment import ExperimentRunner
-from data_manager import CSVDataManager, AdhocDataManager, SyntheticDataManager
 from quantum_infrastructure import QuantumProvider
 from feature_map_factory import FeatureMapFactory
 
@@ -34,40 +33,72 @@ def main():
     print(f"Initialising Experiment Runner in '{EXPERIMENT_MODE}' mode...")
 
     if EXPERIMENT_MODE == 'size':
-        csv = CSVDataManager(filename="mnist_train.csv", target_col="label", num_dims=NUM_DIMS, n_class=N_CLASS)
+        runner.initialise_datasets(
+            num_dims=NUM_DIMS, 
+            experiment_values=[20, 40, 60, 80, 100],
+            mode='size',
+            filename='fashion-mnist_train.csv',
+            target_col='label'
+        )
         runner.run_experiment(
-            mode='size', data_manager=csv, num_dims=NUM_DIMS, num_trials=NUM_TRIALS, 
+            mode='size',num_dims=NUM_DIMS, num_trials=NUM_TRIALS, 
             experiment_values=[20, 40, 60, 80, 100]
         )
         
     elif EXPERIMENT_MODE == 'imbalance':
-        csv = CSVDataManager(filename="breast-cancer.csv", target_col="diagnosis", num_dims=NUM_DIMS, n_class=N_CLASS)
+        runner.initialise_datasets(
+            num_dims=NUM_DIMS,
+            experiment_values=[0.5, 0.6, 0.7, 0.8, 0.9],
+            mode='imbalance',
+            filename='breast-cancer.csv',
+            target_col='diagnosis'
+        )
         runner.run_experiment(
-            mode='imbalance', data_manager=csv, num_dims=NUM_DIMS, num_trials=NUM_TRIALS, 
+            mode='imbalance', num_dims=NUM_DIMS, num_trials=NUM_TRIALS, 
             experiment_values=[0.5, 0.6, 0.7, 0.8, 0.9], fixed_size=FIXED_SIZE
         )
         
     elif EXPERIMENT_MODE == 'feature_complexity':
+        runner.initialise_datasets(
+            num_dims=NUM_DIMS,
+            experiment_values=[1, 2, 3, 4, 5],
+            mode='feature_complexity',
+        )
         runner.run_experiment(
-            mode='feature_complexity', data_manager=None, num_dims=NUM_DIMS, num_trials=NUM_TRIALS, 
+            mode='feature_complexity', num_dims=NUM_DIMS, num_trials=NUM_TRIALS, 
             experiment_values=[1, 2, 3, 4, 5], fixed_size=FIXED_SIZE
         )
         
     elif EXPERIMENT_MODE == 'margin':
+        runner.initialise_datasets(
+            num_dims=NUM_DIMS,
+            experiment_values=[0.1, 0.5, 1.0, 1.5, 2.0],
+            mode='margin',
+        )
         runner.run_experiment(
-            mode='margin', data_manager=None, num_dims=NUM_DIMS, num_trials=NUM_TRIALS, 
+            mode='margin', num_dims=NUM_DIMS, num_trials=NUM_TRIALS, 
             experiment_values=[0.1, 0.5, 1.0, 1.5, 2.0], fixed_size=FIXED_SIZE
         )
         
     elif EXPERIMENT_MODE == 'clusters':
+        runner.initialise_datasets(
+            num_dims=NUM_DIMS,
+            experiment_values=[1, 2, 3, 4],
+            mode='clusters',
+        )
         runner.run_experiment(
-            mode='clusters', data_manager=None, num_dims=NUM_DIMS, num_trials=NUM_TRIALS, 
+            mode='clusters', num_dims=NUM_DIMS, num_trials=NUM_TRIALS, 
             experiment_values=[1, 2, 3, 4], fixed_size=FIXED_SIZE
         )
         
     elif EXPERIMENT_MODE == 'noise':
+        runner.initialise_datasets(
+            num_dims=NUM_DIMS,
+            experiment_values=[0.0, 0.05, 0.10, 0.15, 0.20],
+            mode='noise',
+        )
         runner.run_experiment(
-            mode='noise', data_manager=None, num_dims=NUM_DIMS, num_trials=NUM_TRIALS, 
+            mode='noise', num_dims=NUM_DIMS, num_trials=NUM_TRIALS, 
             experiment_values=[0.0, 0.05, 0.10, 0.15, 0.20], fixed_size=FIXED_SIZE
         )
      
